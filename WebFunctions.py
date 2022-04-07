ad2db1d750d0e6319f07def90d117421d16dd7ff
Upterry <mattleroy11@gmail.com>
2022-04-07 15:40:24 -0400

    Changed how the information is gathered by using single for loop instead of 3 separate list comprehensions. Old code commented out.
    Signed-off-by: Upterry <mattleroy11@gmail.com>

---
+++ WebFunctions.py
@@ -0,0 +1,32 @@
+url_list = ['https://www.newegg.com/Processors-Desktops/SubCategory/ID-343?Tid=7671', 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709']
+
+class Switcher: # Switches between websites when finished scraping
+
+    def __init__(self, new_url):
+        self.new_url = new_url
+
+    @classmethod
+    def url_switcher(self): # Switches between URLS
+        for url in url_list:
+            return url
+
+
+class NWF: # Newegg
+
+    def __init__(self, cell):
+        self.cell = cell
+
+    @classmethod
+    def get_title(cls, cell):
+        title = [item.find(class_="item-title").get_text().split('-')[0] for item in cell]
+        return title
+
+    @classmethod
+    def get_link(cls, cell):
+        link = [item.find('a', href=True)['href'] for item in cell]
+        return link
+
+    @classmethod
+    def get_price(cls, cell):
+        price = [item.find('li', {'class': 'price-current'}).strong.get_text() for item in cell]
+        return price
