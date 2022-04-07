ba7ac516fdb92b4dcf5a61cea658ab1837475ce0
Upterry <mattleroy11@gmail.com>
2022-04-07 15:39:26 -0400

    Changed how the information is gathered by using single for loop instead of 3 separate list comprehensions. Old code commented out.
    Signed-off-by: Upterry <mattleroy11@gmail.com>

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import openpyxl
+from WebFunctions import NWF, Switcher
+import time
 
 # TODO: add list of websites to switch between when one website finishes
 # TODO: add list of urls to switch between on a website when one execution finishes
 
-url_list = ['https://www.newegg.com/CPUs-Processors/Category/ID-34', 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709']
+url_list = ['https://www.newegg.com/Processors-Desktops/SubCategory/ID-343?Tid=7671', 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709']
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36",
     "Accept-Language": "en",
 }
 
-url = "https://www.newegg.com/CPUs-Processors/Category/ID-34"
+#url = "https://www.newegg.com/Processors-Desktops/SubCategory/ID-343?Tid=7671"
 
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.text, "html.parser")
-cell = soup.find_all(class_='item-cell') # This is a list
+
+for url in url_list: # This will need to be modified to fit other websites, currently working with Newegg
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+    cell = soup.find_all(class_='item-cell') # This is a list
+
+    dataset = pd.DataFrame(
+        {
+            'Title': NWF.get_title(cell),
+            'Link': NWF.get_link(cell),
+            'Price': NWF.get_price(cell),
+        })
+
+
+    dataset.to_excel(r'C:\Users\Exo\Documents\Excel\ComputerPartsData.xlsx', index=False)
+    print(dataset.head(10))
+    time.sleep(10)
+
 
 """
 for item in cell:
@@ -29,21 +46,17 @@ for item in cell:
     except AttributeError:
         print("No Price Found")
     print(title, link, price)
-"""
+
 
 title = [item.find(class_="item-title").get_text().split('-')[0] for item in cell] # List comprehension here gets all (full) titles
 link = [item.find('a', href=True)['href'] for item in cell]
 price = [item.find('li', {'class': 'price-current'}).strong.get_text() for item in cell]
+"""
 
-dataset = pd.DataFrame(
-    {
-        'Title': title,
-        'Link': link,
-        'Price': price,
-    })
 
-dataset.to_excel(r'C:\Users\Exo\Documents\Excel\ComputerPartsData.xlsx', index=False)
-print(dataset)
+
+
+
 
 """
 for item in cell:
