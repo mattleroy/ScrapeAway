162f9525b9834016f1f13159c8dc4842775e0bdf
Upterry <mattleroy11@gmail.com>
2022-05-11 16:16:40 -0400

    Now successfully inserts new column data into dataframe with WebFunctions. WebFunctions needs to be updated to pull correct data.
    Signed-off-by: Upterry <mattleroy11@gmail.com>

--- main.py
+++ main.py
@@ -41,22 +41,41 @@ def scrape_data():
         cell = soup.find_all(class_='item-cell')  # This is a list
 
         dataset = pd.DataFrame({
-            'Title': NWF.get_title(cell),   # There is no loop happening here, remember the dataframe data is gathered via
+            #'Title': NWF.get_title(cell),   # There is no loop happening here, remember the dataframe data is gathered via
             'Link': NWF.get_link(cell),     # method within WebFunctions that prints out a list of items from one request
             'Price': NWF.get_price(cell),   # This creates the whole Excel sheet in one go
         })
 
-        def generate_dataframe_data():  # Move function to WebFunctions?
-            data = {"Brand": [NWF.get_brand(url) for url in dataset['Link']],
-                    "Series": [NWF.get_series(url) for url in dataset['Link']],
-                    "Socket": [NWF.get_socket(url) for url in dataset['Link']],
-                    "Cores": [NWF.get_cores(url) for url in dataset['Link']],
-                    "Threads": [NWF.get_threads(url) for url in dataset['Link']],
-                    "Max Frequency": [NWF.get_max_freq(url) for url in dataset['Link']]}
-            return data
+        """
+        data = pd.DataFrame({
+            "a":[1,2,3,4],
+            "b":[5,6,7,8],
+            })
+        
+        data2 = pd.DataFrame({
+            'c':[12,2414141414143,34,45],
+            'd':[56,67,78,89],
+            })
+        
+        data.insert(0,'column_name',data2['c'])
+        """
 
-        dataset2 = pd.DataFrame(generate_dataframe_data())   # Declare dataframe outside of loop. Just add dictionary data to dataframe.
-        dataset2.to_csv("oi")
+        data = {
+                "Brand": [NWF.get_brand(url) for url in dataset['Link']],
+                "Series": [NWF.get_series(url) for url in dataset['Link']],
+                "Socket": [NWF.get_socket(url) for url in dataset['Link']],
+                "Cores": [NWF.get_cores(url) for url in dataset['Link']],
+                "Threads": [NWF.get_threads(url) for url in dataset['Link']],
+                #"Max Frequency": [NWF.get_max_freq(url) for url in dataset['Link']]
+                }
+
+        # Declare dataframe outside of loop. Just add dictionary data to dataframe.
+
+        dataset.insert(0, "Brand", data['Brand'])
+        dataset.insert(1, "Title", data['Series'])
+        dataset.insert(4, "Socket", data['Socket'])
+        dataset.insert(5, "Cores", data['Cores'])
+        dataset.insert(6, "Threads ", data['Threads'])
 
         """In other words, do not form a new DataFrame for each row. Instead,
         collect all the data in a list of dicts, and then call 
@@ -64,9 +83,8 @@ def scrape_data():
         
         https://stackoverflow.com/questions/31674557/how-to-append-rows-in-a-pandas-dataframe-in-a-for-loop
         """
-        #TODO Work on appending dataset2 to dataset1 in correct locations
+
         #TODO Continue debugging get_... WebFunctions
-        #TODO Get other get_functions operational to pull other sub-data (brand, cores, threads, etc)
 
         dataset["Price"].fillna("No Price", inplace=True)  # Fill in Null/None values in the Excel sheet
 
@@ -82,7 +100,7 @@ def scrape_data():
 
 def drop_data():  # This function drops all data within the Excel sheet.
     df = pd.read_excel("ComputerPartsData.xlsx", header=None)
-    df.drop(columns=[0, 1, 2], inplace=True, axis=1)        # Dropping all specified rows (first 3).
+    df.drop(columns=[0, 1, 2, 3, 4, 5, 6], inplace=True, axis=1)        # Dropping all specified rows (first 3).
     with ExcelWriter("ComputerPartsData.xlsx") as writer:
         df.to_excel(writer, index=False, sheet_name="Sheet1")
         # This ExcelWriter is writing "nothing" to the Excel sheet to erase all existing data.
