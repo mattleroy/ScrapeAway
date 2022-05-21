fe3bd6e8cad69e538800e44e12335cc6feffffc6
Upterry <mattleroy11@gmail.com>
2022-05-21 19:03:56 -0400

    Program successfully catches errors, and creates a dictionary which is then appended to a Pandas DataFrame Object. This is written to an Excel sheet.
    Signed-off-by: Upterry <mattleroy11@gmail.com>

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ def scrape_data():
         cell = soup.find_all(class_='item-cell')                                # This is a list of item listings
 
         link_list = NWF.get_link(cell)  # List of links to loop through
-
-
         dictionary_data = []            # List of lists of data - (AMD, Ryzen 5 5600, Socket AM4, etc....)
 
         for url in link_list:           # page_data finds entire page html, get_item_attribute finds item specific html
@@ -46,7 +44,6 @@ def scrape_data():
                 "Max Operating Frequency": [],
                 }
 
-
         for item_attr in dictionary_data:
             try:
                 data["Brand"].append(item_attr[0])
@@ -59,13 +56,16 @@ def scrape_data():
             except IndexError:
                 data["Max Operating Frequency"].append("DNE")
 
-
-
         page_num += 1
 
-scrape_data()
+        dataset = pd.DataFrame(data)  # dataset is the dataframe, data is the dictionary
+
+        with ExcelWriter('ComputerPartsData.xlsx', mode="a", engine="openpyxl", if_sheet_exists="overlay") as writer:
+            dataset.to_excel(writer, startrow=writer.sheets['Sheet1'].max_row, index=False, sheet_name='Sheet1')
+            # arg: startrow=writer.sheets['Sheet1'].max_row is hugely important for appending to the END of the sheet.
+            # Without it, it will not work.
+
 """
-        
         data = pd.DataFrame({
             "a":[1,2,3,4],
             "b":[5,6,7,8],
@@ -77,33 +77,17 @@ scrape_data()
             })
         
         data.insert(0,'column_name',data2['c'])
-        
-
-        # Declare dataframe outside of loop. Just add dictionary data to dataframe.
+"""
 
         #dataset.insert(0, "Brand", data['Brand'])
 
-        In other words, do not form a new DataFrame for each row. Instead,
-        collect all the data in a list of dicts, and then call 
-        df = pd.DataFrame(data) once at the end, outside the loop.
-        
-        https://stackoverflow.com/questions/31674557/how-to-append-rows-in-a-pandas-dataframe-in-a-for-loop
-        
-
         #dataset["Price"].fillna("No Price", inplace=True)  # Fill in Null/None values in the Excel sheet
 
-        with ExcelWriter('ComputerPartsData.xlsx', mode="a", engine="openpyxl", if_sheet_exists="overlay") as writer:
-            dataset.to_excel(writer, startrow=writer.sheets['Sheet1'].max_row, index=False, sheet_name='Sheet1')
-            # arg: startrow=writer.sheets['Sheet1'].max_row is hugely important for appending to the END of the sheet.
-            # Without it, it will not work.
 
-            print(dataset.head(10))     # Can be deleted, just visualizes if data is printing.
-            #time.sleep(10)             # Sleep here so website isn't throttled with requests.
-            page_cap += 1               # Adding 1 to page_cap so while loop ends.
 
 def drop_data():  # This function drops all data within the Excel sheet.
     df = pd.read_excel("ComputerPartsData.xlsx", header=None)
-    df.drop(columns=[0, 1, 2, 3, 4, 5, 6, 7], inplace=True, axis=1)        # Dropping all specified rows (first 3).
+    df.drop(columns=[0, 1, 2, 3, 4, 5, 6, 7], inplace=True, axis=1)        # Dropping all specified rows (first 7).
     with ExcelWriter("ComputerPartsData.xlsx") as writer:
         df.to_excel(writer, index=False, sheet_name="Sheet1")
         # This ExcelWriter is writing "nothing" to the Excel sheet to erase all existing data.
@@ -113,4 +97,3 @@ if df.notnull().values.any():   # This checks if the Excel sheet has existing co
 else:                           # This will populate data if empty.
     scrape_data()
 
-"""
